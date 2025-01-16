f5260508eea3b41ffb9fafb646f5d7d1f477ca8b
geodevcode <geodevinstitute@gmail.com>
2025-01-16 13:37:55 +0100

    aboutus uncommented out

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 
 import LandingPage from "/src/pages/LandingPage";
-// import AboutUs from "/src/pages/Aboutus";
+import AboutUs from "/src/pages/Aboutus";
 import Services from "/src/pages/Services";
 import NotFound from "/src/pages/NotFound";
 import ContactUs from "/src/pages/ContactUs";
@@ -12,7 +12,7 @@ function App() {
     <>
       <Routes location={location} key={location.key}>
         <Route path="/" element={<LandingPage />} />
-        {/* <Route path="/about-us" element={<AboutUs />} /> */}
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/services" element={<Services />} />
 
