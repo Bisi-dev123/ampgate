2a155fbbf3468c12b1cb0c3e835386fa06d1ee21
Olabisi Ajayi <ajayiolabisi.234@gmail.com>
2025-02-02 13:26:16 -0800

    FAQ updated

--- src/components/landingPage/Faq.jsx
+++ src/components/landingPage/Faq.jsx
@@ -10,25 +10,37 @@ export default function Faq() {
         </p>
       </div>
 
-      <div className="border-[1.3px] border-[#262626] rounded-2xl max-w-[650px] mt-16  shadow-[#FF8F06]">
-        <p className="p-4 py-8 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
-          What services does Ampgate Limited offer?
-        </p>
-        <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
-          What types of power equipment are available for sale or rent?
-        </p>
-        <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
-          What industries do you serve?
-        </p>
-        <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
-          How do I request a quote for your services?
-        </p>
-        <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
-          What sets Ampgate Limited apart from competitors?
-        </p>
-        <p className="p-4 py-6 font-bold text-base md:text-lg">
-          Do you provide installation and setup services?
-        </p>
+      <div className="flex justify-center items-center">
+        <div className="border-[1.3px] border-[#262626] rounded-2xl max-w-[650px] mt-16  shadow-[#FF8F06]">
+          <p className="p-4 py-8 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
+            What services does Ampgate Limited offer?
+          </p>
+          <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
+            What types of power equipment are available for sale or rent?
+          </p>
+          <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
+            What industries do you serve?
+          </p>
+          <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
+            How do I request a quote for your services?
+          </p>
+          <p className="p-4 py-6 font-bold text-base md:text-lg border-b-[1.3px] border-[#262626]">
+            What sets Ampgate Limited apart from competitors?
+          </p>
+          <p className="p-4 py-6 font-bold text-base md:text-lg">
+            Do you provide installation and setup services?
+          </p>
+        </div>
+        <div>
+          <p className="font-bold text-3xl mt-2 text-[#161616] ">Do you have more questions?</p>
+          <p>Kindly send an email by clicking on the button below. We shall respond as soon as possible.</p>
+          <button
+          // onClick={() => navigate("/contact-us")}
+          className="p-3 rounded-2xl w-[130px] bg-[#FFB100] hover:bg-gradient-to-b hover:from-[#4a4a4a] hover:to-[#FFB100] transition-all duration-300"
+        >
+          Send an Email
+        </button>
+        </div>
       </div>
     </div>
   );
