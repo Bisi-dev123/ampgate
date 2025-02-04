cae53758155037542de777bcf7aef7ed5be877f4
geodevcode <geodevinstitute@gmail.com>
2025-02-04 14:14:03 +0100

    Testimonial section updated

--- src/components/landingPage/Testimonials.jsx
+++ src/components/landingPage/Testimonials.jsx
@@ -1,19 +1,85 @@
 export default function Testimonials() {
   return (
-    <div className="p-3 pb-14 bg-[#F8F7F3]">
+    <div className="p-3 py-32 bg-[#F8F7F3]">
       <div className="flex flex-col justify-center items-center">
         <p className="border-[1.3px] border-[#000000] p-2.5 px-4 rounded-2xl text-center mb-3 w-[200px]">
           Testimonials
         </p>
-        <p className="">Hear from our satisfied clients</p>
+        <p className="mb-8">Hear from our satisfied clients</p>
       </div>
-
-      <div className="flex ">
-        <div className="">
-          <p className="text-[#FF8F06]">Name of company</p>
-          <p>"Ampgate Limited exceeded our expectations during a critical project. The team delivered top-notch generators and provided seamless installation. Their professionalism and prompt service saved us from downtime. Highly recommended!"</p>
+      <section className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {/* ====== Card One ====== */}
+        <div className="border md:max-w-[380px] text-left p-6 py-10 rounded-xl">
+          <p className="text-[#FF8F06] text-lg">Name of company</p>
+          <p className="text-[#464646] font-thin text-base mt-4">
+            &quot;Ampgate Limited exceeded our expectations during a critical
+            project. The team delivered top-notch generators and provided
+            seamless installation. Their professionalism and prompt service
+            saved us from downtime. Highly recommended!&quot;
+          </p>
+          <div className="flex gap-4 items-center mt-6">
+            <div className="w-16 h-16 flex justify-center items-center bg-black rounded-full">
+              <img
+                src="./why-choose-us1.svg"
+                alt=" Customer Service Icon"
+                className="w-10 h-10 "
+              />
+            </div>
+            <div>
+              <p className="text-[#FFB100] text-base">John Olaiya</p>
+              <p className="text-[#4A4A4A] text-xs">Construction Manager</p>
+            </div>
+          </div>
         </div>
-      </div>
+
+        {/* ====== Card Two ====== */}
+        <div className="border md:max-w-[380px] text-left p-6 py-10 rounded-xl">
+          <p className="text-[#FF8F06] text-lg">Name of company</p>
+          <p className="text-[#464646] font-thin text-base mt-4">
+            &quot;Ampgate Limited exceeded our expectations during a critical
+            project. The team delivered top-notch generators and provided
+            seamless installation. Their professionalism and prompt service
+            saved us from downtime. Highly recommended!&quot;
+          </p>
+          <div className="flex gap-4 items-center mt-6">
+            <div className="w-16 h-16 flex justify-center items-center bg-black rounded-full">
+              <img
+                src="./why-choose-us1.svg"
+                alt=" Customer Service Icon"
+                className="w-10 h-10 "
+              />
+            </div>
+            <div>
+              <p className="text-[#FFB100] text-base">John Olaiya</p>
+              <p className="text-[#4A4A4A] text-xs">Construction Manager</p>
+            </div>
+          </div>
+        </div>
+
+        {/* ====== Card Three ====== */}
+        <div className="border md:max-w-[380px] text-left p-6 py-10 rounded-xl">
+          <p className="text-[#FF8F06] text-lg">Name of company</p>
+          <p className="text-[#464646] font-thin text-base mt-4">
+            &quot;Ampgate Limited exceeded our expectations during a critical
+            project. The team delivered top-notch generators and provided
+            seamless installation. Their professionalism and prompt service
+            saved us from downtime. Highly recommended!&quot;
+          </p>
+          <div className="flex gap-4 items-center mt-6">
+            <div className="w-16 h-16 flex justify-center items-center bg-black rounded-full">
+              <img
+                src="./why-choose-us1.svg"
+                alt=" Customer Service Icon"
+                className="w-10 h-10 "
+              />
+            </div>
+            <div>
+              <p className="text-[#FFB100] text-base">John Olaiya</p>
+              <p className="text-[#4A4A4A] text-xs">Construction Manager</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
