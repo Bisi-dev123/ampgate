58e92f6fe731e87970f4673afc185c9431b7b6e2
geodevcode <geodevinstitute@gmail.com>
2025-02-04 14:12:57 +0100

    FaqData created

---
+++ src/utils/FaqData.js
@@ -0,0 +1,39 @@
+//Faq Section data
+export const faqData = [
+  {
+    id: 1,
+    question: "What services does Ampgate Limited offer?",
+    answer:
+      "Yes. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem similique nulla fugit eum, hic quisquam dolor ",
+  },
+  {
+    id: 2,
+    question: "What types of power equipment are available for sale or rent?",
+    answer:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem similique nulla fugit eum, hic quisquam dolor ",
+  },
+  {
+    id: 3,
+    question: "What industries do you serve?",
+    answer:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem similique nulla fugit eum, hic quisquam dolor ",
+  },
+  {
+    id: 4,
+    question: "How do I request a quote for your services?",
+    answer:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem similique nulla fugit eum, hic quisquam dolor ",
+  },
+  {
+    id: 5,
+    question: "What sets Ampgate Limited apart from competitors?",
+    answer:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem similique nulla fugit eum, hic quisquam dolor ",
+  },
+  {
+    id: 6,
+    question: "Do you provide installation and setup services?",
+    answer:
+      "Yes. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem similique nulla fugit eum, hic quisquam dolor ",
+  },
+];
