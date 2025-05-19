d024d35e3d57fde57fe62da64f52a71dea84b9ce
geodevcode <geodevinstitute@gmail.com>
2025-05-19 21:05:13 +0100

    Improve readability of testimonials by formatting text and correcting typos

--- src/components/landingPage/Testimonials.jsx
+++ src/components/landingPage/Testimonials.jsx
@@ -12,7 +12,10 @@ export default function Testimonials() {
         <div className="border md:max-w-[380px] text-left p-6 py-10 rounded-xl">
           <p className="text-[#FF8F06] font-bold text-lg">CanePro Industries</p>
           <p className="text-[#464646] font-thin text-base mt-4">
-            &quot;Ampgate has been a game-changer for our production facility. Their power solutions are not only reliable but also incredibly efficient. Since partnering with them, we’ve had zero downtime — which is critical for our 24/7 operations. I would recommend!&quot;
+            &quot;Ampgate has been a game-changer for our production facility.
+            Their power solutions are not only reliable but also incredibly
+            efficient. Since partnering with them, we’ve had zero downtime —
+            which is critical for our 24/7 operations. I would recommend!&quot;
           </p>
           <div className="flex gap-4 items-center mt-6">
             <div className="w-14 h-14 flex justify-center items-center bg-black rounded-full">
@@ -24,16 +27,21 @@ export default function Testimonials() {
             </div>
             <div>
               <p className="text-[#FFB100] text-base">David Edmiston</p>
-              <p className="text-[#4A4A4A] text-xs">Oprations Manager</p>
+              <p className="text-[#4A4A4A] text-xs">Operations Manager</p>
             </div>
           </div>
         </div>
 
         {/* ====== Card Two ====== */}
         <div className="border md:max-w-[380px] text-left p-6 py-10 rounded-xl">
-          <p className="text-[#FF8F06] font-bold text-lg">PurpleSquare Developments</p>
+          <p className="text-[#FF8F06] font-bold text-lg">
+            PurpleSquare Developments
+          </p>
           <p className="text-[#464646] font-thin text-base mt-4">
-            &quot;What impressed me most about Ampgate was their dedication to understanding our energy needs. They didn’t just sell a solution; they engineered a system tailored to our properties. Now our tenants enjoy uninterrupted power. You can trust them!&quot;
+            &quot;What impressed me most about Ampgate was their dedication to
+            understanding our energy needs. They didn’t just sell a solution;
+            they engineered a system tailored to our properties. Now our tenants
+            enjoy uninterrupted power. You can trust them!&quot;
           </p>
           <div className="flex gap-4 items-center mt-6">
             <div className="w-14 h-14 flex justify-center items-center bg-black rounded-full">
@@ -54,7 +62,10 @@ export default function Testimonials() {
         <div className="border md:max-w-[380px] text-left p-6 py-10 rounded-xl">
           <p className="text-[#FF8F06] font-bold text-lg">EG&G Hospital</p>
           <p className="text-[#464646] font-thin text-base mt-4">
-            &quot;Power stability is non-negotiable in healthcare, and Ampgate has proven to be a dependable partner. Their backup systems kick in instantly — a critical safeguard for our organization, in terms of patients and equipments. Highly recommended!&quot;
+            &quot;Power stability is non-negotiable in healthcare, and Ampgate
+            has proven to be a dependable partner. Their backup systems kick in
+            instantly — a critical safeguard for our organization, in terms of
+            patients and equipments. Highly recommended!&quot;
           </p>
           <div className="flex gap-4 items-center mt-6">
             <div className="w-14 h-14 flex justify-center items-center bg-black rounded-full">
