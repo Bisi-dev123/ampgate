762eb06ee354257068d66a9c12a0c850844e9222
geodevcode <geodevinstitute@gmail.com>
2025-05-19 18:02:14 +0100

    Fix padding in WhatDrivesUs component for improved layout

--- src/components/landingPage/WhatDrivesUs.jsx
+++ src/components/landingPage/WhatDrivesUs.jsx
@@ -1,6 +1,6 @@
 export default function WhatDrivesUs() {
   return (
-    <div className="p-3 pt-20 lg:pt-32 pb-10 bg-[#F8F7F3]">
+    <div className="p-3 pt-14 lg:pt-20 pb-10 bg-[#F8F7F3]">
       <div className="flex flex-col justify-center items-center">
         <p className="border-[1.3px] border-[#FF8F06] p-2.5 px-4 rounded-2xl text-center mb-3 w-[200px]">
           What Drives Us
